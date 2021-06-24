e864ce1a5eb676187e411b40f2fbe24dd716c767
Shehansha <86175381+automate-shah@users.noreply.github.com>
2021-06-24 19:33:24 +0530

    Added count condition for rg

--- module/provider.tf
+++ module/provider.tf
@@ -12,4 +12,5 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
+  count    = var.resource_group_name == "" ? 1 : 0
 }
